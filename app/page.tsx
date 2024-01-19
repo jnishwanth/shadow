c4d6a28a99873271b1d28a525830fe8445c98e68
Jeya Nishwanth <jeya.nishwanth.j.s@oracle.com>
2024-01-19 18:13:52 +0530

    basic navbar, placeholder pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-import { ThemeToggler } from '@/components/theme-toggler'
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main>
-      <ThemeToggler/>
     </main>
-  )
+  );
 }
